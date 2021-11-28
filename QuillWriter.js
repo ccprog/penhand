0b7eb0ef7b402bbe6fe1f96beba4c1c5a3ec76a5
Claus Colloseus <ccprog@gmx.de>
2021-11-28 18:55:46 +0100

    performance: compute stroke points on a per-stroke basis

--- QuillWriter.js
+++ QuillWriter.js
@@ -1,5 +1,6 @@
 class QuillWriter {
     #config = {
+        delta: undefined,
         width: 10,
         height: 1,
         speed: 200,
@@ -7,14 +8,14 @@ class QuillWriter {
         tilt: -40,
         fill: 'black'
     };
-    #svg;
-    #points;
     #drawing = false;
-    #startTime;
+    #restart;
+    paths = [];
+    #buffer;
 
     constructor(canvas, svg, config) {
         this.ctx = canvas.getContext('2d');
-        this.#svg = svg;
+        this.registerPoints(svg);
         this.config = config;
     }
 
@@ -25,60 +26,120 @@ class QuillWriter {
         const h = this.#config.height / 2;
         const newDelta = Math.min(w, h) / 2;
 
-        this.quill = new Path2D(`M -${w - h / 2},-${h * .8} 
-                               A ${h} ${h * .8} 0 0 0 -${w - h / 2},${h * .8}
-                               Q 0 ${h * 1.6} ${w},${h * .8}
-                               A ${h} ${h * .8} 0 0 0 ${w - h / 2},-${h * .8}
-                               Q 0 -${h * 1.6} -${w - h / 2},-${h * .8} Z`);
+        this.quill = new Path2D(`M -${w - h / 2},-${h * .8}
+                                 A ${h} ${h * .8} 0 0 0 -${w - h / 2},${h * .8}
+                                 Q 0 ${h * 1.6} ${w},${h * .8}
+                                 A ${h} ${h * .8} 0 0 0 ${w - h / 2},-${h * .8}
+                                 Q 0 -${h * 1.6} -${w - h / 2},-${h * .8} Z`);
         this.ctx.fillStyle = this.#config.fill;
 
-        if (newDelta !== this.#config.delta || !this.#points) {
+        if (newDelta !== this.#config.delta) {
             this.#config.delta = newDelta;
             this.ctx.filter = `blur(${newDelta}px)`;
-            this.registerPoints(this.#svg);
+
+            this.paths.forEach(path => delete path.points);
         }
+    }
+
+    get config() {
+        return Object.assign({}, this.#config);
     }
 
     registerPoints(svg) {
         const paths = svg.querySelectorAll('path');
 
-        this.#points = [...paths].map(el => {
-            const length = el.getTotalLength();
-
-            const stroke = {
-                pause: parseInt(el.getAttribute('data-pause'), 10),
-                points: []
-            };
-            let distance = 0;
-            while (distance < length) {
-                stroke.points.push(el.getPointAtLength(distance));
-                distance += this.#config.delta;
+        this.paths = [...paths].map(el => {
+            return {
+                el,
+                length: el.getTotalLength(),
+                pause: parseInt(el.getAttribute('data-pause'), 10)
             }
-            stroke.points.push(el.getPointAtLength(length));
-
-            return stroke;
         });
-
-        const total = this.#points.reduce((sum, stroke) => sum + stroke.points.length, 0);
-        console.log(`ready with ${total} points`);
     }
 
-    start() {
-        return new Promise((resolve, reject) => {
-            if (this.#drawing) {
-                reject(new Error('already running'));
-            } else {
-                this.#drawing = true;
+    async start() {
+        if (this.#drawing) {
+            throw new Error('already running');
+        } else {
+            this.#drawing = true;
 
-                this.ctx.setTransform(1, 0, 0, 1, 0, 0);
-                this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+            this.ctx.setTransform(1, 0, 0, 1, 0, 0);
+            this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
 
-                this.#startTime = performance.now();
-                console.log(`start drawing...`);
+            this.#restart = performance.now();
+            console.log(`start drawing...`);
 
-                requestAnimationFrame(this.#drawProgress.bind(this, 0, 0, 0, resolve));
+            await this.#processPaths();
+
+            this.#drawing = false;
+        }
+    }
+
+    async #processPaths() {
+        for (let stroke of this.paths) {
+            this.#buffer = stroke;
+
+            if (!stroke.points) {
+                const num = this.#getPoints(this.#buffer);
+
+                console.log(`stroke with ${num} points prepared`);
             }
-        });
+
+            stroke.delay = (this.#restart || 0) - performance.now();
+            stroke.perf = [];
+            if (stroke.delay > 0) {
+                await new Promise(resolve => setTimeout(resolve, stroke.delay));
+            } else {
+                this.#restart = performance.now();
+            }
+            console.log(await this.#drawStroke(true));
+        }
+        console.log(await this.#drawStroke(false));
+    }
+
+    #getPoints(stroke) {
+        stroke.points = [];
+
+        let distance = 0;
+
+        while (distance < stroke.length) {
+            stroke.points.push(stroke.el.getPointAtLength(distance));
+            distance += this.#config.delta;
+        }
+
+        stroke.points.push(stroke.el.getPointAtLength(length));
+
+        return stroke.points.length;
+    }
+
+    #drawStroke(more) {
+        if (more) {
+            return new Promise((resolve, reject) => {
+                requestAnimationFrame(this.#drawStep.bind(this, 0, resolve));
+            }).then((result) => {
+                this.#restart = performance.now() + this.#buffer.pause;
+                return Promise.resolve(result);
+            });
+        } else {
+            return Promise.resolve('done');
+        }
+    }
+
+    #drawStep(isAt, resolve, t) {
+        const dur = Math.max(0, (t - this.#restart) / 1000);
+        const len = Math.ceil(dur * this.#config.speed / this.#config.delta);
+
+        const slice = this.#buffer.points.slice(isAt, len);
+        slice.forEach(this.drawAtPoint.bind(this));
+        this.#buffer.perf.push({ dur, isAt, len })
+
+        isAt += slice.length;
+
+        if (isAt >= this.#buffer.points.length) {
+            return resolve(`stroke with ${this.#buffer.points.length} points drawn`);
+        }
+
+        requestAnimationFrame(this.#drawStep.bind(this, isAt, resolve));
     }
 
     drawAtPoint(point) {
@@ -86,34 +147,5 @@ class QuillWriter {
         this.ctx.rotate(this.#config.tilt * Math.PI / 180);
 
         this.ctx.fill(this.quill);
-    }
-
-    #drawProgress(strokeIndex, startsAt, isAt, resolve, t) {
-        const endTime = (t - this.#startTime) / 1000;
-        const end = Math.ceil(endTime * this.#config.speed / this.#config.delta);
-
-        const stroke = this.#points[strokeIndex].points.slice(isAt, end - startsAt);
-        stroke.forEach(this.drawAtPoint.bind(this));
-
-        isAt += stroke.length;
-
-        if (isAt >= this.#points[strokeIndex].points.length) {
-            if (this.#points[strokeIndex + 1]) {
-                const pause = this.#points[strokeIndex].pause || this.#config.pause;
-                this.#startTime += pause;
-
-                startsAt += this.#points[strokeIndex].points.length;
-                strokeIndex++;
-
-                return setTimeout(() => {
-                    requestAnimationFrame(this.#drawProgress.bind(this, strokeIndex, startsAt, 0, resolve));
-                }, pause);
-            } else {
-                this.#drawing = false;
-                return resolve('done');
-            }
-        }
-
-        requestAnimationFrame(this.#drawProgress.bind(this, strokeIndex, startsAt, isAt, resolve));
     }
 }
